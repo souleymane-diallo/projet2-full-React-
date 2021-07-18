e0e9d860d0de68a8476e9ef6dc6ceaf0f269c1fd
John Doe <johndoe@example.com>
2021-07-18 22:35:37 +0200

    Fix Header navigation
    composant  de navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,37 @@
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledSpan = styled.span`
+  padding: 15px;
+  color: #8186a0;
+  font-size: 2em;
+`;
+const StyledLink = styled(Link)`
+  padding: 5px 14px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 1.4em;
+  display: block;
+
+  &:hover {
+    color: white;
+    border-radius: 10px;
+    background-color: #5843e4;
+  }
+`;
 
 function Header() {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
-      <Navbar.Brand href="/" className="mx-5">
-        Paris Event
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/" className="mx-5 logo">
+        <StyledSpan>Paris Event</StyledSpan>
       </Navbar.Brand>
-
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Container>
-          <Nav className="justify-content-center">
-            <Nav.Link href="/">Accueil</Nav.Link>
-            <Nav.Link href="/recherche">Rechercher</Nav.Link>
-            <Nav.Link href="/favoris">Favoris</Nav.Link>
-          </Nav>
-        </Container>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/recherche">Rechercher</StyledLink>
+        <StyledLink to="/favoris">Favoris</StyledLink>
       </Navbar.Collapse>
     </Navbar>
   );
