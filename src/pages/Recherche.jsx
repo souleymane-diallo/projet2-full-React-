695dd49adda6c6f942d60ad3307abd87f1d18fe4
John Doe <johndoe@example.com>
2021-07-18 11:39:54 +0200

    fix page rechercher fonctionne

--- src/pages/Recherche.jsx
+++ src/pages/Recherche.jsx
@@ -1,7 +1,6 @@
-import { Form, Button, Card } from 'react-bootstrap';
-import { useRef, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import DayJS from 'react-dayjs';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useRef, useState } from 'react';
+import CardEvent from '../components/CardEvent';
 
 function Recherche() {
   const inputRef = useRef();
@@ -19,106 +18,39 @@ function Recherche() {
       });
   }
 
-  // ajout d'un événement en favoris dans localStorage
-  const [favorites, setFavorites] = useState(null);
-  useEffect(() => {
-    const eventFavorites = JSON.parse(
-      localStorage.getItem('paris-event-app-favorites')
-    );
-    if (eventFavorites) {
-      setFavorites(eventFavorites);
-    }
-  }, []);
-
-  const saveTolacalStorage = (items) => {
-    localStorage.setItem('paris-event-app-favorites', JSON.stringify(items));
-  };
-
-  const addFavoriteEvent = (event) => {
-    const newFavoriteList = [...favorites, event];
-    setFavorites(newFavoriteList);
-    saveTolacalStorage(newFavoriteList);
-  };
-
-  const removeFavoriteEvent = (event) => {
-    const newFavoriteList = favorites.filter(
-      (favorite) => favorite.record.fields.id !== event.record.fields.id
-    );
-    setFavorites(newFavoriteList);
-    saveTolacalStorage(newFavoriteList);
-  };
-
   return (
     <div className="container">
       <div className="wrapper">
-        <h2 className="my-4">Lister de futurs événements à Paris </h2>
+        <h2 className="my-4 text-center">
+          Lister de futurs événements à Paris{' '}
+        </h2>
         <Form onSubmit={handleSubmit}>
-          <Form.Control
-            className="w-75 my-4"
-            type="text"
-            placeholder="Rechercher un événement"
-            ref={inputRef}
-          />
-          <Button variant="primary" className="px-5" type="submit">
-            Rechercher
-          </Button>
+          <Row>
+            <Col lg="6">
+              <Form.Control
+                type="text"
+                placeholder="Rechercher un événement"
+                ref={inputRef}
+              />
+            </Col>
+            <Col>
+              <Button variant="outline-primary" type="submit">
+                Rechercher
+              </Button>
+            </Col>
+          </Row>
         </Form>
         <hr />
         {records && (
           <>
-            <h3>Résultats de la recherche</h3>
+            <h3 className="text-center">Résultats de la recherche</h3>
             <section className="d-flex flex-wrap">
               {records.length === 0 ? (
-                <p>Aucun resultat pour cette recherche...</p>
+                <p className="text-center">
+                  Aucun resultat pour cette recherche...
+                </p>
               ) : (
-                records.map((record) => (
-                  <Link
-                    className="Link"
-                    to={`event/${record.record.id}`}
-                    key={record.record.id}
-                  >
-                    <Card
-                      className="image-container"
-                      style={{ width: '18rem' }}
-                    >
-                      <Card.Img
-                        variant="top"
-                        src={record.record.fields.cover_url}
-                      />
-                      <Card.Body>
-                        <Card.Title>{record.record.fields.title}</Card.Title>
-                        <p>
-                          <DayJS format="DD / MM / YYYY à hh : mm : ss">
-                            {record.record.fields.date_start}
-                          </DayJS>
-                        </p>
-                        <Card.Text
-                          dangerouslySetInnerHTML={{
-                            __html: record.record.fields.description.substring(
-                              0,
-                              120
-                            ),
-                          }}
-                        />
-                      </Card.Body>
-                      <div className="p-3" onClick={addFavoriteEvent}>
-                        <svg
-                          width="1em"
-                          height="1em"
-                          viewBox="0 0 16 16"
-                          className="bi bi-heart-fill"
-                          fill="red"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                          />
-                        </svg>
-                      </div>
-                    </Card>
-                  </Link>
-                ))
+                records.map((record) => <CardEvent record={record} />)
               )}
             </section>
           </>
