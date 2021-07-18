46e54ffb87b6972ccec41063c5d6cb6a93207ccc
John Doe <johndoe@example.com>
2021-07-18 22:34:30 +0200

    fix
    composant main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Accueil from './pages/Accueil';
+import Recherche from './pages/Recherche';
+import Favoris from './pages/Favoris';
+import Header from './components/Header/Header';
+import EventDetails from './pages/EventDetails/EventDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Route exact path="/">
+          <Accueil />
+        </Route>
+        <Route path="/recherche">
+          <Recherche />
+        </Route>
+        <Route path="/favoris">
+          <Favoris />
+        </Route>
+        <Route path="/event/:id">
+          <EventDetails />
+        </Route>
+      </Router>
+    </>
   );
 }
 
