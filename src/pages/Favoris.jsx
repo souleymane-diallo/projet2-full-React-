e08b57d61b1ec99896e6fb6f6c8fc710627c877f
John Doe <johndoe@example.com>
2021-07-18 20:07:03 +0200

    Page Favoris Erreur non resolue

---
+++ src/pages/Favoris.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import CardEvent from '../components/CardEvent';
+
+function Favoris() {
+  const [favorisEvent, setFavorisEvent] = useState([]);
+
+  /**
+   * Afficher un évenement dans la page favoris
+   */
+  useEffect(() => {
+    setFavorisEvent(JSON.parse(localStorage.getItem('Eventfavorites')));
+    console.log(favorisEvent);
+  }, []);
+
+  return (
+    <section className="container">
+      <div className="wrapper">
+        <h2 className="my-3">Evénement Sauvegardés</h2>
+        {favorisEvent && favorisEvent.length === 0 ? (
+          <p>Aucun événement n'a été Sauvegardé</p>
+        ) : (
+          favorisEvent.map((favori) => (
+            <CardEvent key={favori.id} record={favori} />
+          ))
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Favoris;
